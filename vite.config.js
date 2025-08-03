2ac36a3ec8a19990f9883cc97d25b53a1382588c
Arianna Billeci <176613378+aribi-95@users.noreply.github.com>
2025-08-03 17:50:40 +0200

    Modifica impostazioni per deploy

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+    base: "/EpiBooks/",
     plugins: [react()],
     test: {
         environment: "jsdom",
