13f1d4adbb75690c2ff14a7973ba5602bb9ca72b
Arianna Billeci <176613378+aribi-95@users.noreply.github.com>
2025-08-03 18:21:19 +0200

    Update stile NotFound.jsx

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,16 +1,21 @@
 import { Col, Container, Row } from "react-bootstrap";
 import sadCat from "../assets/404-sad-cat.png";
+import { ThemeContext } from "../context/ThemeContext";
+import { useContext } from "react";
 
 function NotFound() {
+    const { theme } = useContext(ThemeContext);
+    const titleThemeClass = theme === "dark" ? "title-dark" : "title-light";
+
     return (
         <Container className="mt-4">
-            <h2 className="text-center">
+            <h2 className={`text-center ${titleThemeClass}`}>
                 Oh, no!
                 <br />
                 Sembra che tu abbia navigato su una rotta non disponibile.
             </h2>
-            <Row className="justify-content-center">
-                <Col md={8}>
+            <Row className="justify-content-center mt-4">
+                <Col md={8} lg={6} xl={4}>
                     <img src={sadCat} alt="Sad cat" className="w-100" />
                 </Col>
             </Row>
