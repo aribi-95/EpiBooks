15be2c86cb0b3ee0be5989fda7962b82510cdbc4
Arianna Billeci <176613378+aribi-95@users.noreply.github.com>
2025-08-03 18:11:10 +0200

    Pagina Not Found aggiornata

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,5 +1,21 @@
+import { Col, Container, Row } from "react-bootstrap";
+import sadCat from "../assets/404-sad-cat.png";
+
 function NotFound() {
-    return <h1>not found</h1>;
+    return (
+        <Container className="mt-4">
+            <h2 className="text-center">
+                Oh, no!
+                <br />
+                Sembra che tu abbia navigato su una rotta non disponibile.
+            </h2>
+            <Row className="justify-content-center">
+                <Col md={8}>
+                    <img src={sadCat} alt="Sad cat" className="w-100" />
+                </Col>
+            </Row>
+        </Container>
+    );
 }
 
 export default NotFound;
